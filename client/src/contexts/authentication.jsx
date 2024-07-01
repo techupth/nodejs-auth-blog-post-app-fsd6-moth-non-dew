0ef0a9a34da3935975e6d7e8d993c39a4f860129
KeeratiNon <keeratinon3@gmail.com>
2024-07-01 22:29:03 +0700

    resolve code

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {jwtDecode} from "jwt-decode"
+import axios from "axios"
 
 
 const AuthContext = React.createContext();
